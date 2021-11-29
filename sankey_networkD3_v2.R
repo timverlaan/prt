34c1d20e307466e3633449e3a6dadd91b8bb102b
timverlaan <timverlaan@live.nl>
2021-11-29 17:45:19 +0100

    Split the composite cats

---
+++ sankey_networkD3_v2.R
@@ -0,0 +1,37 @@
+library(networkD3)
+library(data.table)
+
+PRT_sankey <- read_excel("~/PRT/prt/PRT_sankey.xlsx")
+
+d <- PRT_sankey[, c(1, 4, 5)]
+
+# df <- read.csv(text = "Study,Category,Class
+# study17,cat H,class B;class C
+# study32,cat A;cat B,class A
+# study7,cat F,class A
+# study21,cat F,class C
+# study24,cat F,class B;class C
+# study15,cat E;cat K,class C")
+setDT(d)
+
+# make links
+links <- rbind(d[, .(source = Study, target = Category) ],
+               d[, .(source = Category, target = Class) ])
+links[, rn := .I]
+# adjust value, based on "split"
+links <- links[, strsplit(source, split = ";", fixed = TRUE), by = .(source, target, rn)
+][, .(source = V1, target, rn)
+][, strsplit(target, split = ";", fixed = TRUE), by = .(source, target, rn) 
+][, .(source, target = V1, rn)
+][, .(source, target, value = 1/.N), by = rn]
+# make nodes
+nodes <- data.frame(name = unique(unlist(links[,.(source, target)])))
+nodes$label <- nodes$name
+
+# update link ids
+links$source_id <- match(links$source, nodes$name) - 1
+links$target_id <- match(links$target, nodes$name) - 1
+
+# plot
+sankeyNetwork(Links = links, Nodes = nodes, Source = 'source_id',
+              Target = 'target_id', Value = 'value', NodeID = 'label')
